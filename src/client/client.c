118cf8a045da3f6f74af152b0d560554c468c1bb
Paris Hom <homm.paris@gmail.com>
2020-11-21 17:19:08 -0800

    added file transfer from a server to client

--- src/client/client.c
+++ src/client/client.c
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) 2008 Bob Beck <beck@obtuse.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
-/* client.c  - the "classic" example of a socket client */
 #include <arpa/inet.h>
 
 #include <netinet/in.h>
@@ -31,38 +14,65 @@
 #include <string.h>
 #include <unistd.h>
 
-
+#include <tls.h>
+// functions for TLS: 
+// tls_init(void): 
+		// The tls_init() function initializes global data structures. 
+		// It is no longer necessary to call this function directly, since it is invoked internally when needed. 
+		// It may be called more than once, and may be called concurrently.
+// struct tls_config* tls_config_new(void):
+		// create configuration before connection is created
+		// allocaties, init, and returns new default configuration for future connections
+		// --> tls_config_set_protocols(3), tls_load_file(3), tls_config_ocsp_require_stapling(3), and tls_config_verify(3).
+// const char* tls_config_error(struct tls_config *config);
+		// used to retrieve a string contiang more info about most recent error relating to a configurations
+// tls connection object is created by tls_client (or tls_server) and configured with tls_configure
+// client connection is init after configuration by calling tls_connect. server accepts new client connection with tls_accept _socket on already established socket connection
+// input - tls_read, output - tls_write
+// after use, tls connection closed with tls_close and then freed with tls_free.
 
 static void usage()
 {
-	extern char * __progname;
-	fprintf(stderr, "usage: %s ipaddress portnumber\n", __progname);
+	extern char *__progname;
+	fprintf(stderr, "usage: %s -port proxyportnumber filename\n", __progname);
 	exit(1);
 }
 
+// your application name -port proxyportnumber filename
 int main(int argc, char *argv[])
 {
-	struct sockaddr_in server_sa;
-	char buffer[80], *ep;
-	size_t maxread;
+
+	int clientSocket, ret;
+	struct sockaddr_in serverAddr;
+	char buffer[1024], *ep;
+	char fileName[1024];
 	ssize_t r, rc;
 	u_short port;
 	u_long p;
 	int sd;
 
-	if (argc != 3)
-		usage();
+	ssize_t len;
+	int fileSize;
+	FILE *recievedFile;
+	int remainingData = 0;
 
-        p = strtoul(argv[2], &ep, 10);
-        if (*argv[2] == '\0' || *ep != '\0') {
+	if (argc != 4) // not enough arguments passed in
+	{
+		usage();
+	}
+
+	p = strtoul(argv[2], &ep, 10); // grab proxyportnumber
+	if (*argv[2] == '\0' || *ep != '\0')
+	{
 		/* parameter wasn't a number, or was empty */
 		fprintf(stderr, "%s - not a number\n", argv[2]);
 		usage();
 	}
-        if ((errno == ERANGE && p == ULONG_MAX) || (p > USHRT_MAX)) {
+	if ((errno == ERANGE && p == ULONG_MAX) || (p > USHRT_MAX))
+	{
 		/* It's a number, but it either can't fit in an unsigned
-		 * long, or is too big for an unsigned short
-		 */
+		* long, or is too big for an unsigned short
+		*/
 		fprintf(stderr, "%s - value out of range\n", argv[2]);
 		usage();
 	}
@@ -70,57 +80,103 @@ int main(int argc, char *argv[])
 	port = p;
 
 	/*
-	 * first set up "server_sa" to be the location of the server
-	 */
-	memset(&server_sa, 0, sizeof(server_sa));
-	server_sa.sin_family = AF_INET;
-	server_sa.sin_port = htons(port);
-	server_sa.sin_addr.s_addr = inet_addr(argv[1]);
-	if (server_sa.sin_addr.s_addr == INADDR_NONE) {
-		fprintf(stderr, "Invalid IP address %s\n", argv[1]);
-		usage();
+	* first set up "serverAddr" to be the location of the server
+	*/
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	// grab the filename from argument
+	strcpy(fileName, argv[3]);
+	printf("[+]File Name: %s\n", fileName);
+
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket < 0)
+	{
+		printf("[-]Error in connection.\n");
+		exit(1);
+	}
+	// from bob-beck libtls tutorial
+	// after you call connect, you call tls_connect_socket to associate a tls context with your connected socket
+	// struct tls *ctx; 
+	// struct tls_config *config;
+	// config = tls_config_new();
+	// if ((ctx = tls_client()) == NULL) { err(1, "[-]Failed to create tls_client\n"); }
+	// if (tls_configure(ctx, config) == -1 ) { err(1, "[-]Failed to configure: %s", tls_error(ctx));}
+	printf("[+]Client Socket is created.\n");
+
+
+	ret = connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	// grab a socket
+	// if (tls_connect(ctx, "127.0.0.1", argv[2]) == -1) {
+	// 	printf("[-]Error on tls_connect_socket\n");
+	// 	exit(1);
+	// }
+	printf("[+]Connected to Server.\n");
+
+	// tls_handshake();
+
+	while (1)
+	{
+		ssize_t written, w;
+		w = 0;
+		written = 0;
+		while (written < strlen(fileName))
+		{
+			w = write(clientSocket, fileName+written, strlen(fileName) - written);
+			// w = tls_write(ctx, fileName+written, strlen(fileName) - written);
+			if (w == -1)
+			{
+				if (errno != EINTR)
+					err(1, "write failed\n");
+			}
+			else
+				written += w;
+		}
+
+		printf("[+]Sent Proxy file name: %s\n", fileName);
+
+		if (strcmp(buffer, ":exit") == 0)
+		{
+			// if (tls_close(ctx) == -1) {errx(1, "failed to close: %s", tls_error(ctx));}
+			close(clientSocket);
+			printf("[-]Disconnected from server.\n");
+		}
+		// recieve the file size
+		if (recv(clientSocket, buffer, sizeof(buffer), 0) < 0)
+		{
+			printf("[-]Error in receiving data.\n");
+		}
+		else
+		{
+			fileSize = atoi(buffer);
+			recievedFile = fopen(fileName, "w");
+			if (recievedFile == NULL)
+			{
+				fprintf(stderr, "[-]Failed to open file foo --> %s\n", strerror(errno));
+
+				exit(EXIT_FAILURE);
+			}
+			remainingData = fileSize;
+			while ((remainingData > 0) && ((len = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0))
+			{
+				fwrite(buffer, sizeof(char), len, recievedFile);
+				remainingData -= len;
+				printf("[+]Received %d bytes. Waiting on: %d bytes\n", (int)len, remainingData);
+			}
+			if (remainingData <= 0) {
+				printf("[+]Finished receiving file. Closing Socket.\n");
+				fclose(recievedFile);
+				close(clientSocket);
+				exit(0);
+			}
+			else {
+				printf("[-]Something went wrong!\n");
+				exit(1);
+			}
+		}
 	}
 
-	/* ok now get a socket. we don't care where... */
-	if ((sd=socket(AF_INET,SOCK_STREAM,0)) == -1)
-		err(1, "socket failed");
-
-	/* connect the socket to the server described in "server_sa" */
-	if (connect(sd, (struct sockaddr *)&server_sa, sizeof(server_sa))
-	    == -1)
-		err(1, "connect failed");
-
-	/*
-	 * finally, we are connected. find out what magnificent wisdom
-	 * our server is going to send to us - since we really don't know
-	 * how much data the server could send to us, we have decided
-	 * we'll stop reading when either our buffer is full, or when
-	 * we get an end of file condition from the read when we read
-	 * 0 bytes - which means that we pretty much assume the server
-	 * is going to send us an entire message, then close the connection
-	 * to us, so that we see an end-of-file condition on the read.
-	 *
-	 * we also make sure we handle EINTR in case we got interrupted
-	 * by a signal.
-	 */
-	r = -1;
-	rc = 0;
-	maxread = sizeof(buffer) - 1; /* leave room for a 0 byte */
-	while ((r != 0) && rc < maxread) {
-		r = read(sd, buffer + rc, maxread - rc);
-		if (r == -1) {
-			if (errno != EINTR)
-				err(1, "read failed");
-		} else
-			rc += r;
-	}
-	/*
-	 * we must make absolutely sure buffer has a terminating 0 byte
-	 * if we are to use it as a C string
-	 */
-	buffer[rc] = '\0';
-
-	printf("Server sent:  %s",buffer);
-	close(sd);
-	return(0);
+	return 0;
 }
---
+++ src/proxy/proxy.c
@@ -0,0 +1,201 @@
+#include <arpa/inet.h>
+
+#include <netinet/in.h>
+
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+
+#include <tls.h> // for TLS
+
+static void usage()
+{
+	extern char *__progname;
+	fprintf(stderr, "usage: %s -port portnumber\n", __progname);
+	exit(1);
+}
+
+static void kidhandler(int signum)
+{
+	/* signal handler for SIGCHLD */
+	waitpid(WAIT_ANY, NULL, WNOHANG);
+}
+
+// your application name -port portnumber
+int main(int argc, char *argv[])
+{
+
+	// relative to this proxy
+	int sockfd, ret;
+	struct sockaddr_in serverAddr;
+	char buffer[1024], *ep;
+	u_long p;
+	u_short port;
+
+	// for any new connections
+	int newSocket;
+	struct sockaddr_in newAddr;
+	socklen_t addr_size;
+	pid_t childpid;
+	pid_t pid;
+
+	// get the portnumber from argument
+	if (argc != 3)
+	{
+		usage();
+	}
+
+	errno = 0;
+	p = strtoul(argv[2], &ep, 10); // grab proxy port number
+	if (*argv[2] == '\0' || *ep != '\0')
+	{
+		/* parameter wasn't a number, or was empty */
+		fprintf(stderr, "%s - not a number\n", argv[2]);
+		usage();
+	}
+	if ((errno == ERANGE && p == ULONG_MAX) || (p > USHRT_MAX))
+	{
+		/* It's a number, but it either can't fit in an unsigned
+		* long, or is too big for an unsigned short
+		*/
+		fprintf(stderr, "%s - value out of range\n", argv[2]);
+		usage();
+	}
+	/* now safe to do this */
+	port = p;
+
+
+//	// libtls setup
+	// struct tls *ctx;
+	// struct tls_config *config; 
+	// config = tls_config_new();
+	// char ca_file[1024] = "../../certificates/root/certs/ca.cert.pem"; // certificate for server, not sure if this is the right one to use
+	// tls_config_set_ca_file(config, ca_file);
+	// ctx = tls_server(); // set up server context
+	// tls_configure(ctx,config);
+
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		printf("[-]Error in connection.\n");
+		exit(1);
+	}
+	printf("[+]Proxy Socket is created.\n");
+
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	if (ret < 0)
+	{
+		printf("[-]Error in binding.\n");
+		exit(1);
+	}
+	printf("[+]Bind to port %d\n", port);
+
+	if (listen(sockfd, 10) == 0)
+	{
+		printf("[+]Listening....\n");
+	}
+	else
+	{
+		printf("[-]Error in binding.\n");
+	}
+
+	while (1)
+	{
+		printf("[+]Accepting new connections..\n");
+		newSocket = accept(sockfd, (struct sockaddr *)&newAddr, &addr_size);
+		if (newSocket < 0)
+		{
+			exit(1);
+		}
+		printf("[+]Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+		// struct tls **cctx;
+		// if (tls_accept_socket(ctx, cctx, newSocket) == -1){
+			// 	printf("[-]Error on tls_accept_socket\n");
+			// 	exit(1);
+		// }
+		if ((childpid = fork()) == 0)
+		{
+			close(sockfd);
+
+			while (1)
+			{
+				ssize_t msgLength;
+				if ((msgLength = recv(newSocket, buffer, sizeof(buffer), 0)) <= 0)
+				{ // check to see if client closed connection
+					printf("[-]Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+					break;
+				}
+				else if (strcmp(buffer, ":exit") == 0)
+				{
+					printf("[-]Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+					break;
+				}
+				else // sending the file back to the user.
+				{
+
+					int fd;
+					char fileContent[1024], c;
+					buffer[msgLength] = '\0'; // make sure that we only look at the message we read in
+					printf("[+]Client requests: %s, read %d bytes from buffer \n", buffer, (int)msgLength);
+					// find the file from filename
+					if ((fd = open(buffer, O_RDONLY)) == -1)
+					{
+						printf("[-]Error! opening file\n");
+						// Program exits if the file pointer returns NULL.
+						strncpy(buffer,"File does not exist.", sizeof(buffer));
+						send(newSocket,buffer, sizeof(buffer), 0);
+						break;
+					}
+					printf("[+]Opened file.\n");
+
+					struct stat fileStat;
+					if (fstat(fd, &fileStat) < 0)
+					{
+						printf("[-]Error at fstat\n");
+						exit(1);
+					}
+					char fileSize[256];
+					sprintf(fileSize, "%d", (int)fileStat.st_size);
+					int len;
+					if ((len = send(newSocket, fileSize, sizeof(fileSize), 0) < 0))
+					{
+						printf("[-]Fail on sending length of file size");
+						exit(1);
+					}
+
+					off_t offset = 0;
+					int remainingData = fileStat.st_size;
+					// sending file data
+					int sentBytes = 0;
+					while ((sentBytes = sendfile(newSocket, fd, &offset, BUFSIZ)) > 0 && remainingData > 0)
+					{
+						remainingData -= sentBytes;
+					}
+					printf("[+]Finished sending file to client\n");
+					bzero(buffer, sizeof(buffer));
+				}
+			}
+		}
+	}
+	close(newSocket);
+
+	return 0;
+}
